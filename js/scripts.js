2682dbc7d7dab1c397d311b66e72bab14cd2abc4
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-08-05 17:12:39 -0500

    add script for pork ribs image modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,8 @@ $(function () {
     $("#hotLinksBtn").on('click', function () {
         $("#hotLinksModal").modal('show');
     });
-    
+    $("#porkSpareRibsBtn").on('click', function () {
+        $("#porkSpareRibsModal").modal('show');
+    });
+
 });
